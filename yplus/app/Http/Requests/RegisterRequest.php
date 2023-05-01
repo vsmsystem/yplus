7a52af96d3663c5480d15d496616a6e11284c05d
Valdecir Merli <vsm_system@hotmail.com>
2023-05-01 13:31:28 -0300

    test fixing username again

--- yplus/app/Http/Requests/RegisterRequest.php
+++ yplus/app/Http/Requests/RegisterRequest.php
@@ -25,7 +25,7 @@ class RegisterRequest extends FormRequest
     {
         return [
             'email' => 'required|email:rfc,dns|unique:users,email',
-            'name' => 'required|unique:users,username',
+            'name' => 'required|unique:users,name',
             'password' => 'required|min:8',
             'password_confirmation' => 'required|same:password'
         ];
