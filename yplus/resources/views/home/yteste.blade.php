33ea94d5a376777398bb96c3d7d10c2cd8387c9c
Valdecir Merli <vsm_system@hotmail.com>
2023-05-04 19:54:35 -0300

    favicon

--- yplus/resources/views/home/yteste.blade.php
+++ yplus/resources/views/home/yteste.blade.php
@@ -4,10 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="Karmanta - Bootstrap 3 Responsive Admin Template">
-  <meta name="author" content="GeeksLabs">
-  <meta name="keyword" content="Karmanta, Dashboard, Admin, Template, Theme, Bootstrap, Responsive, Retina, Minimal">
-  <link rel="shortcut icon" href="img/favicon2.png">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <link rel="icon" href="img/favicon.ico" />
 
   <title>Yplus</title>
 
