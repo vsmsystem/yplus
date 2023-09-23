0905ecba6c0078a9e04b52ddf7af150812560035
Valdecir Merli <vsm_system@hotmail.com>
2023-09-22 21:36:22 -0300

    favicon adjustment

--- yplus/resources/views/layouts/app-master.blade.php
+++ yplus/resources/views/layouts/app-master.blade.php
@@ -7,7 +7,15 @@
   <meta name="description" content="">
   <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
   <meta name="generator" content="Hugo 0.87.0">
-  <title>Fixed top navbar example · Bootstrap v5.1</title>
+
+  <link rel="apple-touch-icon" sizes="180x180" href="{{asset('img/favicon/apple-touch-icon.png')}}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{asset('img/favicon/favicon-32x32.png')}}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{asset('img/favicon/favicon-16x16.png')}}">
+  <link rel="manifest" href="{{asset('/faviconimg/site.webmanifest')}}">
+  <link rel="mask-icon" href="{{asset('img/favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+  <title>Yplus</title>
 
   <!-- Bootstrap core CSS -->
   <link href="{!! url('assets/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
