e3bcbae0acd2ce47d5d3afb75c017d6375314660
Valdecir Merli <vsm_system@hotmail.com>
2023-09-22 23:19:58 -0300

    icon background color

--- yplus/resources/views/auth/login.blade.php
+++ yplus/resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
   <link rel="manifest" href="{{asset('/faviconimg/site.webmanifest')}}">
   <link rel="mask-icon" href="{{asset('img/favicon/safari-pinned-tab.svg')}}" color="#5bbad5">
   <meta name="msapplication-TileColor" content="#da532c">
-  <meta name="theme-color" content="#ffffff">
+  <meta name="theme-color" content="#000000">
   
   <title>Yplus</title>
   <style>
