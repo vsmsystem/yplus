94154645e04b46949a3eb2954cf1b346be180f09
Valdecir Merli <vsm_system@hotmail.com>
2023-09-22 23:05:53 -0300

    assets and icons

--- yplus/resources/views/layouts/main.blade.php
+++ yplus/resources/views/layouts/main.blade.php
@@ -734,31 +734,31 @@
     </nav>
 
     <!-- javascripts -->
-    <script src="js/jquery.js"></script>
-    <script src="js/jquery-1.8.3.min.js"></script>
-    <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
+    <script src="{{asset('js/jquery.js')}}"></script>
+    <script src="{{asset('js/jquery-1.8.3.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/jquery-ui-1.9.2.custom.min.js')}}"></script>
     <!-- bootstrap -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
     <!-- nice scroll -->
-    <script src="js/jquery.scrollTo.min.js"></script>
-    <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="{{asset('js/jquery.scrollTo.min.js')}}"></script>
+    <script src="{{asset('js/jquery.nicescroll.js')}}" type="text/javascript"></script>
     <!-- charts scripts -->
-    <script src="assets/jquery-knob/js/jquery.knob.js"></script>
-    <script src="js/jquery.sparkline.js" type="text/javascript"></script>
-    <script src="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
-    <script src="js/owl.carousel.js"></script>
+    <script src="{{asset('assets/jquery-knob/js/jquery.knob.js')}}"></script>
+    <script src="{{asset('js/jquery.sparkline.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js')}}"></script>
+    <script src="{{asset('js/owl.carousel.js')}}"></script>
     <!-- jQuery full calendar -->
-    <script src="assets/fullcalendar/fullcalendar/fullcalendar.min.js"></script>
+    <script src="{{asset('assets/fullcalendar/fullcalendar/fullcalendar.min.js')}}"></script>
     <!--script for this page only-->
-    <script src="js/calendar-custom.js"></script>
+    <script src="{{asset('js/calendar-custom.js')}}"></script>
     <!-- custom select -->
-    <script src="js/jquery.customSelect.min.js"></script>
+    <script src="{{asset('js/jquery.customSelect.min.js')}}"></script>
     <!--custome script for all page-->
-    <script src="js/scripts.js"></script>
+    <script src="{{asset('js/scripts.js')}}"></script>
     <!-- custom script for this page-->
-    <script src="js/sparkline-chart.js"></script>
-    <script src="js/easy-pie-chart.js"></script>
-    <script src="app.js"></script>
+    <script src="{{asset('js/sparkline-chart.js')}}"></script>
+    <script src="{{asset('js/easy-pie-chart.js')}}"></script>
+    <script src="{{asset('app.js')}}"></script>
 
     <script>
         //knob
