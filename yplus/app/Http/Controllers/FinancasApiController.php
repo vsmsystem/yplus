72e5b9ed98aae4b23a79eac0045139623b1e4df1
anaharumi <ana.harumi.1989@gmail.com>
2023-12-13 11:11:23 -0300

    Validations inserted in Financial Transactions

--- yplus/app/Http/Controllers/FinancasApiController.php
+++ yplus/app/Http/Controllers/FinancasApiController.php
@@ -12,25 +12,32 @@ class FinancasApiController extends Controller
         return Financas::all();
     }
 
-    public function criarTransacao(Request $request)
-{
-    $data = $request->json()->all();
 
-    $transacao = Financas::create([
-        'additional_info' => $data['additional_info'],
-        'amount' => $data['amount'],
-        'url' => $data['url'],
-        'issue_date' => $data['date'],
-    ]);
+    public function criarTransacao(Request $request){
+        $data = $request->json()->all();
 
+        $validator = Validator::make($data, [
+            'additional_info' => 'required|string',
+            'amount' => 'integer',
+            'date' => 'date',
+            'url' => 'nullable|url',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
 
-    return response()->json(['message' => 'Transação criada com sucesso', 'data' => $transacao], 201);
-}
+        $transacao = new Financas();
+        $transacao->additional_info = $data['additional_info'];
+        $transacao->amount = $data['amount'];
+        $transacao->url = $data['url'];
+        $transacao->issue_date = $data['date'];
+
+        $transacao->save();
+
+        return response()->json(['message' => 'Transação criada com sucesso', 'data' => $transacao], 201);
+    }
     
-
-
-
 }
 
 
